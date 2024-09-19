38099d2e7190d824d78a035cb4eb643e4336e6af
Diego Huamani <dhuamanilu@unsa.edu.pe>
2024-09-19 18:53:54 -0500

    Agregando boton para crear cuenta,se le dio funcionalidad y se modifico el activity account xml porque algunos elementos no se mostraban bien

--- app/src/main/java/com/example/loginsample/LoginActivity.java
+++ app/src/main/java/com/example/loginsample/LoginActivity.java
@@ -2,6 +2,7 @@ package com.example.loginsample;
 
 import static androidx.constraintlayout.helper.widget.MotionEffect.TAG;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -9,15 +10,11 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 import com.example.loginsample.databinding.ActivityMainBinding;
 
-public class MainActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
     private ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,6 +33,7 @@ public class MainActivity extends AppCompatActivity {
         EditText edtUsername = binding.edtUsername;
         EditText edtPassword = binding.edtPassword;
         Button btnLogin = binding.btnLogin;
+        Button btnAddAccount = binding.btnAddAccount;
         btnLogin.setOnClickListener(new View.OnClickListener(){
 
             @Override
@@ -50,6 +48,10 @@ public class MainActivity extends AppCompatActivity {
                     Log.d(TAG,"Error en la autenticacion");
                 }
             }
+        });
+        btnAddAccount.setOnClickListener(v->{
+            Intent intent = new Intent(getApplicationContext(),AccountActivity.class);
+            startActivity(intent);
         });
     }
 }
