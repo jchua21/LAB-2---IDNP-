c18058065b0359d122fde6a79c006779f5c36a5a
Diego Huamani <dhuamanilu@unsa.edu.pe>
2024-09-19 18:18:53 -0500

    Primer commit, se uso el proyecto anterior , Creando la clase AccountEntity para modelar los datos de un usuario

---
+++ app/src/main/java/com/example/loginsample/AccountEntity.java
@@ -0,0 +1,58 @@
+package com.example.loginsample;
+
+public class AccountEntity {
+    private string firstname;
+    private string lastname;
+    private string email;
+    private string phone;
+    private string username;
+    private string password;
+
+    public string getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(string firstname) {
+        this.firstname = firstname;
+    }
+
+    public string getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(string lastname) {
+        this.lastname = lastname;
+    }
+
+    public string getEmail() {
+        return email;
+    }
+
+    public void setEmail(string email) {
+        this.email = email;
+    }
+
+    public string getPhone() {
+        return phone;
+    }
+
+    public void setPhone(string phone) {
+        this.phone = phone;
+    }
+
+    public string getUsername() {
+        return username;
+    }
+
+    public void setUsername(string username) {
+        this.username = username;
+    }
+
+    public string getPassword() {
+        return password;
+    }
+
+    public void setPassword(string password) {
+        this.password = password;
+    }
+}
